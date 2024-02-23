f1a52cc6108e404a84775e209808cc587af81af3
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-02-23 12:55:54 +0100

    fix relationship between reviews and places

--- models/place.py
+++ models/place.py
@@ -21,5 +21,5 @@ class Place(BaseModel, Base):
     amenity_ids = []
     user = relationship("User", back_populates="places")
     cities = relationship("City", back_populates="places")
-    # reviews = relationship("Review", back_populates="place",
-    # cascade="all, delete_orphan")
+    reviews = relationship("Review", back_populates="place",
+                           cascade="all, delete_orphan")
