5c38eb7d1b2a85994ed9dab3c7d05f39c3c925ca
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-04-10 05:53:33 +0100

    web static deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
  #!/usr/bin/python3
+=======
+#!/usr/bin/python3
+"""full deployment module"""
+from AirBnB_clone_V2 import 1-pack_web_static
+
+>>>>>>> afcea8e (web static deployment)
