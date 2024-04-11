047be74284d281114e22e1d4e611c6ebc91adf00
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-04-11 05:28:47 +0100

    full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
 """distributes an archive to your web servers,
 using the function do_deploy """
-from fabric.api import local, put, env, run
+from fabric.api import put, env, run, local
+from datetime import datetime
 from os import path
 
-""" establishing connection with th host """
+""" establishing connection with the host """
 env.hosts = ['web-01.judithsolutions.tech','web-02.judithsolutions.tech']
 env.user = "ubuntu"
 env.key_filename = "~/.ssh/id_rsa"
 
+def do_pack():
+    """ do pack module """
+    try:
+        local(f"mkdir -p versions")
+        now = datetime.now().strftime('%Y%m%d%H%M%S')
+        file_name = f"web_static_{now}.tgz"
+        local(f"tar -czvf versions/{file_name} web_static")
+        return f"versions/{file_name}"
+    except Exception as e:
+        return None
+
 def do_deploy(archive_path):
-    """ deployment     """
+    """ deployment """
     try:
         # path existence
         if not path.exists(archive_path):
@@ -35,6 +47,12 @@ def do_deploy(archive_path):
         # remove prev symbolic link and direct to new sym link
         run(f"sudo rm -rf /data/web_static/current")
         run(f"sudo ln -s {remote_directory_two} /data/web_static/current")
-        return True
     except:
         return False
+    return True
+
+def deploy():
+    """ full deployment """
+    archive_path = do_pack()
+    result = do_deploy(archive_path)
+    return (result)
