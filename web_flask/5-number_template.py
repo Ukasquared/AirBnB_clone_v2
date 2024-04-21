32e2ea8ecc2763439be4962a6271d93c3db7a9bb
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-04-21 18:31:02 +0100

    serve html page using flask

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" create a minimal program
+using flask the accept user input """
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """displays on browser"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ displays on browser"""
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def user_input(text):
+    """ recieves user input to display"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return (f"C {text}")
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def second_input(text):
+    if "_" in text:
+        text = text.replace('_', ' ')
+    return (f"Python {text}")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def accept_int(n):
+    return (f"{n} is a number")
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def serve_html(n):
+    return (render_template('5-number.html', n=n))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
